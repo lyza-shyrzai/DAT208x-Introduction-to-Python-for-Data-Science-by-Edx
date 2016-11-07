53812bddce31a959bc2ada244ff517e985c35315
LizaSh <lizash605@gmail.com>
2016-11-07 21:13:00 +0200

    Finished a Packages exercises

---
+++ Functions and Packages/Selective_import.py
@@ -0,0 +1,11 @@
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon if 12 degrees. Store in dist.
+dist = r * radians(12)
+
+# Print out dist
+print(dist)
