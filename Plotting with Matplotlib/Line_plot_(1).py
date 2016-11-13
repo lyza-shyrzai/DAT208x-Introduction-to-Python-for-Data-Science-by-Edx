d4cecc5040425f9bb66494d2a2e918fb6fa6f495
LizaSh <lizash605@gmail.com>
2016-11-13 22:25:01 +0200

    Finished exercises from Basic Plots with matplotlib

---
+++ Plotting with Matplotlib/Line_plot_(1).py
@@ -0,0 +1,10 @@
+# Print the last item from year and pop
+print(year[-1])
+print(pop[-1])
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+plt.show()
