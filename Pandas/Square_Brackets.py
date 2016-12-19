eec0db776dd93034c8c69dbcdde630b9fe2bb6e4
LizaSh <lizash605@gmail.com>
2016-12-19 23:58:04 +0200

    Finished exercises from Pandas

---
+++ Pandas/Square_Brackets.py
@@ -0,0 +1,9 @@
+#Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
---
+++ Pandas/loc(1).py
@@ -0,0 +1,9 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+print(cars.loc['JAP'])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
