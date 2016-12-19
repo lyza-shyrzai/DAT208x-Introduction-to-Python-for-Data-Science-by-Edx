56bbc0a8baf77d97ae9a7b426f5afc4836bde934
LizaSh <lizash605@gmail.com>
2016-12-19 23:22:05 +0200

    Finished exercises from Boolean Logic and Control Flow

---
+++ Bollean Logic and Contol Flow/Equality.py
@@ -0,0 +1,11 @@
+# Comparison of booleans
+print(True == False)
+
+# Comparison of integers
+print(-5 * 15 != 75)
+
+# Comparison of strings
+print("pyscript" == "PyScript")
+
+# Compare a boolean with an integer
+print(True == 1)
